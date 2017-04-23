cd00c1c8ca81d7c178bc7326caba91dc5560027a
huangshaohua <huangshaohuamail@gmail.com>
2017-04-23 20:40:19 +0800

    暂时不进一步about的开发

--- app/src/main/java/com/example/owen/weathergo/modules/about/About.java
+++ app/src/main/java/com/example/owen/weathergo/modules/about/About.java
@@ -1,8 +1,25 @@
 package com.example.owen.weathergo.modules.about;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.owen.weathergo.R;
+
 /**
  * Created by owen on 2017/4/8.
  */
 
-public class About {
+public class About extends AppCompatActivity {
+
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+    public static void launch(Context context) {
+        context.startActivity(new Intent(context, About.class));
+    }
 }
