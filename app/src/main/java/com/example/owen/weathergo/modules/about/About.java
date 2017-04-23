4dcee3aa4e6b64d1a9bccd07b7f5752f2f4f1d8a
huangshaohua <huangshaohuamail@gmail.com>
2017-04-23 22:24:01 +0800

    利用CoordinatorLayout布局about

--- app/src/main/java/com/example/owen/weathergo/modules/about/About.java
+++ app/src/main/java/com/example/owen/weathergo/modules/about/About.java
@@ -3,7 +3,10 @@ package com.example.owen.weathergo.modules.about;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import com.example.owen.weathergo.R;
 
@@ -11,7 +14,7 @@ import com.example.owen.weathergo.R;
  * Created by owen on 2017/4/8.
  */
 
-public class About extends AppCompatActivity {
+public class About extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     public void onCreate( Bundle savedInstanceState) {
@@ -21,5 +24,10 @@ public class About extends AppCompatActivity {
 
     public static void launch(Context context) {
         context.startActivity(new Intent(context, About.class));
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
     }
 }
